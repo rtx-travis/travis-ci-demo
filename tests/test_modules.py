21722fcd77b2ac3359153fb9803edd732500cb46
finnagin <flan313@gmail.com>
2021-03-31 12:03:39 -0700

    added module test

---
+++ tests/test_modules.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import scipy as sci
+import pandas as pd
+
+
+def test_numpy():
+    assert np.__version__ == '1.18.5'
+
+def test_pandas():
+    assert np.__version__ == '1.0.4'
+
+def test_scipy():
+    assert np.__version__ == '1.4.1'
