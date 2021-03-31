2b68da14c3335834a7cb99d63c006276d95f9bb7
finnagin <flan313@gmail.com>
2021-03-31 13:02:56 -0700

    fixed modules

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -6,10 +6,10 @@ import pandas as pd
 
 
 def test_numpy():
-    assert np.__version__ == '1.18.5'
+    assert np.__version__ == '1.19.2'
 
 def test_pandas():
-    assert np.__version__ == '1.0.4'
+    assert np.__version__ == '1.0.5'
 
 def test_scipy():
-    assert np.__version__ == '1.4.1'
+    assert np.__version__ == '1.6.0'
